409c0fd24ceb82a5c0e83ef6048dc2ccd3f82b78
ramaxx95 <ramilozano@live.com.ar>
2023-12-01 16:43:15 -0300

    corrijo parametros pasados en orden erroneo

--- development/infrastructure/src/gateways/gateway.tsx
+++ development/infrastructure/src/gateways/gateway.tsx
@@ -27,9 +27,15 @@ export default class ApiGateway implements IGateway {
 
     async getEvents(...args: any[]): Promise<EventDto[]> {
 
-        const config = this.getAxiosConfig("get", "/api/events/search", ["id", "name", "category", "location", "date_from", "date_to", "edition", "athlete_first_name", "athlete_last_name", "athlete_country"], args);
+        console.log("[DEBUG] args...")
+        console.log(args)
+
+        const config = this.getAxiosConfig("get", "/api/events/search", ["id", "name", "category", "location", "edition", "date_from", "date_to", "athlete_first_name", "athlete_last_name", "athlete_country"], args);
 
         const response = await axios(config);
+
+        console.log("[DEBUG] response...")
+        console.log(response.data.results)
 
         const events = response.data.results.map((x: {[k: string]: string}) =>
             {
