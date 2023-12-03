7150835d15857bdbdb421e5b060e51876e5657f0
ramaxx95 <ramilozano@live.com.ar>
2023-12-03 16:20:00 -0300

    fix typo recoverPassword

--- development/web/app/recover_password/actions.tsx
+++ development/web/app/recover_password/actions.tsx
@@ -1,13 +1,15 @@
-import { PasswordRecoverUseCaseImpl } from '../../src/server-container';
+'use server'
+
+import {PasswordRecoverUseCaseImpl} from "@/src/server-container";
 
 export async function recoverPassword(prevState: any, formData: FormData) {
   try {
     PasswordRecoverUseCaseImpl.mail = formData.get('email')?.toString();
-    PasswordRecoverUseCaseImpl.recoverUrl = 'https://grupo-5.2023.tecnicasdedisenio.com.ar/change_password';
-    //PasswordRecoverUseCaseImpl.recoverUrl = 'http://localhost:35000/change_password';
+    PasswordRecoverUseCaseImpl.recoverUrl = "http://localhost:35000/change_password";
+    //useCase.recoverUrl = 'https://grupo-5.2023.tecnicasdedisenio.com.ar/change_password';
     return await PasswordRecoverUseCaseImpl.handle();
   } catch (e) {
     console.error(e);
-    return { message: 'Failed to create user' }
+    return { message: 'Failed to send mail' }
   }
 }
