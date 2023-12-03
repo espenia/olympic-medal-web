409d343ad42aa7d22e757b882649b3d0fcafeb31
Tomas Dahab <tomidahab@gmail.com>
2023-12-03 18:41:19 -0300

    uso getUsers en vez de getUser

--- development/web/app/validate/actions.tsx
+++ development/web/app/validate/actions.tsx
@@ -1,11 +1,25 @@
 'use server';
-import { GetClassificationUseCaseImpl, AcceptClassificationUseCaseImpl,DeclineClassificationUseCaseImpl, GetUserUseCaseImpl } from "@/src/server-container";
+import { getServerSession } from 'next-auth';
+import { GetClassificationUseCaseImpl, AcceptClassificationUseCaseImpl,DeclineClassificationUseCaseImpl, GetUserUseCaseImpl, GetUsersUseCaseImpl } from "@/src/server-container";
+import UserDto from '../../../entities/users/user';
 
-export async function getUser() {
+/*export async function getUser() {
     const user = await GetUserUseCaseImpl.handle();
 
     return user;
-}
+}*/
+
+export async function getUser() {
+    const session = await getServerSession();
+    GetUsersUseCaseImpl.id = undefined;
+    GetUsersUseCaseImpl.firstName = undefined;
+    GetUsersUseCaseImpl.lastName = undefined;
+    GetUsersUseCaseImpl.email = session?.user?.email ?? undefined;
+  
+    const users = await GetUsersUseCaseImpl.handle();
+  
+    return users && users.length > 0 ? users.at(0)! : new UserDto();
+  }
 
 export async function getClassification(firstName: string | undefined, lastName: string | undefined) {
     GetClassificationUseCaseImpl.firstName = firstName;
